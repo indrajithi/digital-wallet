177432f82d43e3e4b2366ff3426b51023b23fe68
Indrajith Indraprastham <mail@indrajith.me>
2024-04-21 20:29:20 +0530

    create model User

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
